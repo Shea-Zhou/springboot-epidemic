0b6887390247d57698a7c87db5e1ca1cc14f8f94
Rhea-ZhouY <Rhea_ZhouY@163.com>
2020-05-08 16:54:46 +0800

    第一次提交 工具类获取疫情数据

---
+++ epidemic/src/main/java/com/rhea/epidemic/domain/DataBean.java
@@ -0,0 +1,17 @@
+package com.rhea.epidemic.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Administrator Rhea
+ */
+@Data@AllArgsConstructor@NoArgsConstructor
+public class DataBean {
+    private String area;
+    private int newConfirm;
+    private int confirm;
+    private int heal;
+    private int dead;
+}
---
+++ epidemic/src/main/java/com/rhea/epidemic/util/DataHandler.java
@@ -0,0 +1,57 @@
+package com.rhea.epidemic.util;
+
+import com.google.gson.Gson;
+import com.rhea.epidemic.domain.DataBean;
+
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Administrator Rhea
+ * 工具类 读取疫情数据
+ */
+public class DataHandler {
+    public static void main(String[] args) throws Exception {
+        List<DataBean> data = getData();
+        System.out.println(data);
+    }
+
+    private static List<DataBean> getData() throws Exception {
+        FileReader reader = new FileReader("tmp.txt");
+        char[] cBuf = new char[1024];
+        int cRead ;
+        StringBuilder builder = new StringBuilder();
+        while ((cRead=reader.read(cBuf))>0){
+            builder.append(new String(cBuf,0,cRead));
+        }
+        reader.close();
+        //把json格式转换为Java
+        Gson gson = new Gson();
+        Map map = gson.fromJson(builder.toString(), Map.class);
+        //分别获取转换后的内容中的地区名字 疫情统计
+        String data = map.get("data").toString();
+        Map dataMap = gson.fromJson(data, Map.class);
+        //获取地区集合
+        ArrayList list = (ArrayList) dataMap.get("areaTree");
+        //获取中国的数据集合
+        Map areaMap = (Map) list.get(0);
+        //获取省份
+        ArrayList provenceList = (ArrayList) areaMap.get("children");
+        List<DataBean> dataBeanList = new ArrayList<>();
+        //遍历循环省份集合 获取省份的名字
+        for (Object o : provenceList) {
+            Map temp = (Map) o;
+            String name = (String) temp.get("name");
+            Map totalMap = (Map) temp.get("total");
+            double nowConfirm = (double) totalMap.get("nowConfirm");
+            double confirm = (double) totalMap.get("confirm");
+            double heal = (double) totalMap.get("heal");
+            double dead = (double) totalMap.get("dead");
+            DataBean dataBean = new DataBean(name, (int) nowConfirm, (int) confirm, (int) heal, (int) dead);
+            dataBeanList.add(dataBean);
+        }
+        return dataBeanList;
+    }
+}
